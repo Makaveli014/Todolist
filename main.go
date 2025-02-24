21f554d5bfcbb43a37c034db3cb6441e49271f06
Makavelli014 <kgdmitriev@gmail.com>
2025-02-24 22:46:01 +0300

    Первый коммит проекта Todolist

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"Todolist/routes"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger" // new
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(logger.New())
+
+	// setup routes
+	setupRoutes(app) // new
+
+	// Listen on server 8000 and catch error if any
+	err := app.Listen(":8000")
+
+	// handle error
+	if err != nil {
+		panic(err)
+	}
+}
+func setupRoutes(app *fiber.App) {
+	// give response when at /
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"success": true,
+			"message": "You are at the endpoint 😉",
+		})
+	})
+
+	// api group
+	api := app.Group("/api")
+
+	// give response when at /api
+	api.Get("", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"success": true,
+			"message": "You are at the api endpoint 😉",
+		})
+	})
+
+	// connect todo routes
+	routes.TodoRoute(api.Group("/todos"))
+}
